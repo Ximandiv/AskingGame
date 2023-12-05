008ee7b304acec0efcac14fe36770ad0e9fcd070
Emmanuel Vanegas Carmona <emmanuel.vanegas@doctus.com.co>
2023-12-05 16:37:34 -0500

    Initial Commit
    Adds all of the elements already worked on

---
+++ Primero - Juego Preguntas/JS/script.js
@@ -0,0 +1,64 @@
+let timeFinished;
+let timeInterval;
+
+function StartTimer(){
+    timeFinished = setTimeout(finishGame, 31000);
+    timeInterval = setInterval(DecreaseSeconds, 1000);
+}
+
+function DecreaseSeconds(){
+    let timer = document.getElementById('timer');
+
+    let output = String(Number(timer.textContent) - 1).padStart(2, '0');
+
+    timer.textContent = output;
+}
+
+function finishGame(){
+    StopTimer();
+
+    let audio = document.getElementById('gameoverAudio');
+
+    buttonFinish.removeAttribute('hidden');
+    button.hidden = true;
+    
+    audio.play();
+    alert('Game Over! Se ha acabado el tiempo.')
+}
+
+function EndGame(){
+    StopTimer();
+
+    let timeNow = new Date();
+    timeNow = timeNow.toLocaleString('es-Es');
+    let buttonFinish = document.getElementById('buttonFinish');
+    let button = document.getElementById('button');
+    let answerOne = document.getElementById('answerOne').value;
+    let answerTwo = document.getElementById('answerTwo').value;
+    let answerThree = document.getElementById('answerThree').value;
+    let answerFour = document.getElementById('answerFour').value;
+    let answerFive = document.getElementById('answerFive').value;
+
+    let output = `
+    Estás son tus respuestas
+    Respuesta Uno: ${answerOne}
+    Respuesta Dos: ${answerTwo}
+    Respuesta Tres: ${answerThree}
+    Respuesta Cuatro: ${answerFour}
+    Respuesta Cinco: ${answerFive}
+    Fecha y Hora: ${timeNow}`;
+
+    buttonFinish.removeAttribute('hidden');
+    button.hidden = true;
+
+    alert(output);
+}
+
+function Reset(){
+    location.reload();
+}
+
+function StopTimer(){
+    clearTimeout(timeFinished);
+    clearInterval(timeInterval);
+}
